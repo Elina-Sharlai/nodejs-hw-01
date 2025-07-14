6b83f35d596ae571641843161f51e786a5e93f79
Elina-Sharlai <elinakometa@gmail.com>
2025-07-14 17:36:45 +0300

    done first Node-HW

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,18 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  const contacts = await readContacts();
+
+  if (contacts.length === 0) {
+    console.log("The contact doesn't exist.");
+    return;
+  }
+
+  const removed = contacts.pop();
+  await writeContacts(contacts);
+
+  console.log('Last contact has been deleted:', removed);
+};
 
 removeLastContact();
